1063df2f86397de2c1760f475dae5a9624c73e2d
Bianca <biancancanal@gmail.com>
2021-05-04 10:40:11 -0500

    Draft

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,24 @@
 // Code SimpleComponent Here
+
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+    constructor() {
+        super()
+        this.state = {
+            mood: "happy"
+        }
+    }
+    
+    handleClick = () => {
+        //callback to div click event 
+    }
+
+    render() {
+        return (
+            <div onClick={this.state.mood}>
+            </div>
+        )
+    }
+}
+
